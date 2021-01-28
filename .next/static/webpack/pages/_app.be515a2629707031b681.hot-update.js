3a2351d657acc870fb351cde9068ac3b07ae17ba
Adriano <adriano.aluizi@xmobots.com.br>
2021-01-27 22:51:58 -0300

    Desenvolver layout

---
+++ .next/static/webpack/pages/_app.be515a2629707031b681.hot-update.js
@@ -0,0 +1,14 @@
+webpackHotUpdate_N_E("pages/_app",{
+
+/***/ "./db.json":
+/*!*****************!*\
+  !*** ./db.json ***!
+  \*****************/
+/*! exports provided: bg, title, description, questions, external, theme, default */
+/***/ (function(module) {
+
+eval("module.exports = JSON.parse(\"{\\\"bg\\\":\\\"https://www.wallpaperup.com/uploads/wallpapers/2013/10/29/166945/b04488af38585a2f5a510c6d20ee36a6-700.jpg\\\",\\\"title\\\":\\\"Quiz Next\\\",\\\"description\\\":\\\"Teste\\\",\\\"questions\\\":[{\\\"image\\\":\\\"https://media.giphy.com/media/xUOxf3yDKCuwpOlT3i/giphy.gif\\\",\\\"title\\\":\\\"Como fazer um seletor por id via CSS?\\\",\\\"description\\\":\\\"Essa é pra aquecer\\\",\\\"answer\\\":2,\\\"alternatives\\\":[\\\".elemento\\\",\\\"*elemento\\\",\\\"#elemento\\\",\\\"%elemento\\\"]},{\\\"image\\\":\\\"https://media.giphy.com/media/13FrpeVH09Zrb2/giphy.gif\\\",\\\"title\\\":\\\"Como fazer um background gradiente com css?\\\",\\\"description\\\":\\\"Faz tempo em haha\\\",\\\"answer\\\":0,\\\"alternatives\\\":[\\\"background: linear-gradient(#e66465, #9198e5);\\\",\\\"background: gradient(#e66465, #9198e5);\\\"]}],\\\"external\\\":[\\\"\\\"],\\\"theme\\\":{\\\"colors\\\":{\\\"primary\\\":\\\"#f57c00\\\",\\\"secondary\\\":\\\"#7600b2\\\",\\\"mainBg\\\":\\\"#f79633\\\",\\\"contrastText\\\":\\\"#FFFFFF\\\",\\\"wrong\\\":\\\"#aa00ff\\\",\\\"success\\\":\\\"#4CAF50\\\"},\\\"borderRadius\\\":\\\"4px\\\"}}\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL2RiLmpzb24uanMiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./db.json\n");
+
+/***/ })
+
+})
